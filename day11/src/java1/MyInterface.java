4e756bc58ef6584eb5245c3d12eae7afae5bf5a0
‘mxg133’ <396330646@qq.com>
2020-06-16 10:34:55 +0800

    2020.6.16

---
+++ day11/src/java1/MyInterface.java
@@ -0,0 +1,10 @@
+package java1;
+
+/**
+ * @author shkstart
+ * @create 2019 下午 3:15
+ */
+
+public interface MyInterface {
+    void info();
+}
---
+++ day12/src/java4/Girl.java
@@ -0,0 +1,34 @@
+package java4;
+
+/**
+ * @author shkstart
+ * @create 2019 下午 7:23
+ */
+public class Girl {
+
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Girl() {
+
+    }
+
+    public Girl(String name) {
+
+        this.name = name;
+    }
+}
